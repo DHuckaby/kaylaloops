04fb58a2e84bac3c4088c76117c3a5ef5d671389
Daniel Huckaby <daniel@huckaby.dev>
2026-04-20 11:37:09 -0500

    Automate header modification

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ##### Deploy
 - rebuild.sh
-- edit docs/index and update `header{height:0vh}` (all three)
 - push, github actions will deploy
 
 ##### References
--- rebuild.sh
+++ rebuild.sh
@@ -3,3 +3,6 @@ cp ./docs/CNAME ./dist/
 cp ./docs/.nojekyll ./dist/
 rm -rf ./docs/
 mv ./dist/ ./docs/
+
+# Automate fixed header
+sed -i '' 's/\(header{[^}]*height:\)[0-9]*vh/\10vh/g' docs/index.html
